77e494295bb4cf1ef0eae350d2e42dbf30e848ba
ikost <ikost@LENOVO-KOSTOV>
2016-12-22 01:16:55 +0200

    22.Dec 01:16

---
+++ Blog/Controllers/Admin/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers.Admin
+{
+    public class UserController : Controller
+    {
+        
+        // GET: User/List
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var users = database.Users.ToList();
+
+                return View(users);
+            }
+        }
+
+        //GET: User
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+    }
+}
